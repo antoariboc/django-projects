182390e80559233c9619a10e0b060f880ccd74ca
antoariboc <antoariboc@gmail.com>
2025-04-02 10:57:19 +0200

    .

--- bookstore/accounts/models.py
+++ bookstore/accounts/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 
+
+# Sin hacer esto daba error "AttributeError at /accounts/login/'Manager' object has no attribute 'get_by_natural_key'"
+class CustomUserManager(BaseUserManager):
+    def get_by_natural_key(self, username):
+        return self.get(username=username)
+
 class CustomUser(AbstractBaseUser):
+    objects = CustomUserManager() # Lo mismo que el comentario de arriba
     
     uid = models.UUIDField(default=None, blank=True, null=True, unique=True)
     
     USERNAME_FIELD = 'uid'
     email = models.CharField(max_length=30, default=None)
     username = models.CharField(max_length=30, default=None)
+    
--- bookstore/accounts/views.py
+++ bookstore/accounts/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
 
 def login(request):
     template_data = {}
@@ -19,4 +26,20 @@ def login(request):
         return render(request, 'accounts/login.html',{'template_data': template_data})
     else:
         auth_login(request, user)
-        return redirect('home.index')
+        return redirect('home')
+    
+    
+def signup(request):
+    template_data = {}
+    template_data['title'] = 'Sign Up'
+    if request.method == 'GET':
+        template_data['form'] = CustomUserCreationForm()
+        return render(request, 'accounts/signup.html', {'template_data': template_data})
+    elif request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts.login')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html',{'template_data': template_data})
