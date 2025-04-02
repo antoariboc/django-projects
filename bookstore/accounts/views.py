ab6da30444c68731a96257721adad3a16cbb3be3
antoariboc <antoariboc@gmail.com>
2025-04-02 08:28:17 +0200

    .

--- bookstore/accounts/views.py
+++ bookstore/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 
 # Create your views here.
+
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',{'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+        request,
+        username = request.POST['username'],
+        password = request.POST['password']
+    )
+    if user is None:
+        template_data['error'] ='The username or password is incorrect.'
+        return render(request, 'accounts/login.html',{'template_data': template_data})
+    else:
+        auth_login(request, user)
+        return redirect('home.index')
